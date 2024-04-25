66cd6c2ba0c54ffe25230c7879b6629a5b2c9ade
asadaravani <abegano1@stu.vistula.edu.pl>
2024-04-25 22:00:56 +0600

    Initial commit

---
+++ src/test/java/kg/beganov/AuthProject/AuthProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.beganov.AuthProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
